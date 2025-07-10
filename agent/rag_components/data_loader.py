ca8989e7b6a2b80b3088ccbe4bb08a9f8671f791
den1shh <dengud79@gmail.com>
2025-07-10 19:21:22 +0300

    added docs, tuned data_loader

--- agent/rag_components/data_loader.py
+++ agent/rag_components/data_loader.py
@@ -10,9 +10,11 @@ def load_and_chunk_documents(file_path: str) -> List:
     loader = TextLoader(file_path, encoding='utf-8')
     documents = loader.load()
     text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000,
-    chunk_overlap=200,
-    separators=["\n\n", "\n", " ", ""]
+        chunk_size=1600,
+        chunk_overlap=200,
+        separators=[
+            "\n# ", "\n## ", "\n- ", "\n\n", "\n", " ", ""
+        ]
     )
     chunked_docs = text_splitter.split_documents(documents)
     print(f"Документ '{file_path}' разбит на {len(chunked_docs)} чанков.")
